cbe36d999cb7aa3cdf1423a3512c055f1960c758
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2016-02-11 09:48:26 +0100

    Blog//Ajout CSS et JS//Blog

--- vue/blog/inc/footer.php
+++ vue/blog/inc/footer.php
@@ -1,2 +1,4 @@
+  <script src="vue/blog/js/jquery.js"></script>
+  <script src="vue/blog/js/bootstrap.js"></script>
 </body>
 </html>
--- vue/blog/index.php
+++ vue/blog/index.php
@@ -1,12 +1,12 @@
 <?php
 include 'inc/header.php';
 ?>
-  <h1 class="centered">Bienvenue sur mon premier blog !</h1>
+  <h1 class="centered">Derniers articles publiés :</h1>
     <?php
     foreach($billets as $billet){
       ?>
       <div class="articles">
-        <h3 class="centered titre">
+        <h3 class="text-center titre">
           <?php echo $billet['titre']; ?>
           <br/>
           <em>Le <?php echo $billet['date_creation']; ?></em>
